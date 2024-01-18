02ac2d3f2f8c52823e770cc2f422ed7f935f6304
André Mion <andremion@gmail.com>
2024-01-18 14:15:54 +0000

    Bump Android version to 1.0.1 (2)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.andremion.jobster.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
